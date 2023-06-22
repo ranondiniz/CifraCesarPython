23062dba3def2139eace538fe995dacf3f22253c
Ranon Diniz <115851280+ranondiniz@users.noreply.github.com>
2023-06-22 18:20:17 -0300

    Add files via upload

---
+++ depuracao/debug-caesar-3.py
@@ -0,0 +1,54 @@
+# Bug do programa #3
+#
+
+# Duplique o alfabeto dado
+def getDoubleAlphabet(alphabet):
+    doubleAlphabet = alphabet + alphabet
+    return doubleAlphabet
+
+# Obter uma mensagem para criptografar
+def getMessage():
+    stringToEncrypt = input("Please enter a message to encrypt: ")
+    return stringToEncrypt
+
+# Obter uma chave da cifra
+def getCipherKey():
+    shiftAmount = input("Please enter a key (whole number from 1-25): ")
+    return shiftAmount
+
+# Criptografar mensagem
+def encryptMessage(message, cipherKey, alphabet):
+    encryptedMessage = ""
+    uppercaseMessage = ""
+    uppercaseMessage = message.upper()
+    for currentCharacter in uppercaseMessage:
+        position = alphabet.find(currentCharacter)
+        newPosition = position + int(cipherKey)
+        if currentCharacter in alphabet:
+            encryptedMessage = encryptedMessage + alphabet[newPosition]
+        else:
+            encryptedMessage = encryptedMessage + currentCharacter
+    return encryptedMessage
+
+# Descriptografar mensagem
+def decryptMessage(message, cipherKey, alphabet):
+    decryptKey = -1 * int(cipherKey)
+    return encryptMessage(message, cipherKey, alphabet)
+
+# Main principal
+def runCaesarCipherProgram():
+    myAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    print(f'Alphabet: {myAlphabet}')
+    myAlphabet2 = getDoubleAlphabet(myAlphabet)
+    print(f'Alphabet2: {myAlphabet2}')
+    myMessage = getMessage()
+    print(myMessage)
+    myCipherKey = getCipherKey()
+    print(myCipherKey)
+    myEncryptedMessage = encryptMessage(myMessage, myCipherKey, myAlphabet2)
+    print(f'Encrypted Message: {myEncryptedMessage}')
+    myDecryptedMessage = decryptMessage(myEncryptedMessage, myCipherKey, myAlphabet2)
+    print(f'Decrypted Message: {myDecryptedMessage}')
+
+# Main 
+runCaesarCipherProgram()
